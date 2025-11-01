c3a30a74806ffe5f46de12ac38d6948cea1567f6
vfarcy <internet@farcy.me>
2025-11-01 11:53:22 +0100

    trace

--- web/static/js/openfsd/login.js
+++ web/static/js/openfsd/login.js
@@ -13,11 +13,28 @@ $("#login-form").on("submit", (ev) => {
         dataType: "json",
         data: JSON.stringify(requestBody)
     }).then((res) => {
-        console.log(res);
+        console.log("API login response:", res);
+        if (res && res.err) {
+            alert("Erreur d'authentification: " + res.err);
+            console.error("Erreur API:", res.err);
+            return;
+        }
+        if (!res || !res.data || !res.data.access_token) {
+            alert("Réponse API inattendue. Veuillez réessayer.");
+            console.error("Réponse API inattendue:", res);
+            return;
+        }
         localStorage.setItem("access_token", res.data.access_token)
         localStorage.setItem("refresh_token", res.data.refresh_token)
         window.location.href = "/dashboard"
     }).fail((xhr) => {
-        alert(`${xhr.statusText}: ${xhr.responseJSON['err']}`)
+        let errMsg = "Erreur inconnue";
+        if (xhr.responseJSON && xhr.responseJSON.err) {
+            errMsg = xhr.responseJSON.err;
+        } else if (xhr.statusText) {
+            errMsg = xhr.statusText;
+        }
+        alert(`Échec de la connexion: ${errMsg}`);
+        console.error("Échec AJAX login:", xhr);
     })
 })
